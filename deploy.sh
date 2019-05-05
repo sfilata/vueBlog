f2c7ea79feb614809c554bb8d1863daeddab8bef
Neil <shubaiqiao@126.com>
2019-05-05 18:15:42 +0800

    feat: 修正不正确的部署命令

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:sfilata/sfilata.github.io.git masters
+git push -f git@github.com:sfilata/sfilata.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:sfilata/vueBlog.git master
